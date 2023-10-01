53a24aea3c17803846d0ed42c56a38b4240c6ac5
akanniquadree <akanniquadry7@gmail.com>
2023-10-01 14:24:22 +0100

    Stripe checkout payment

--- Routes/User.js
+++ Routes/User.js
@@ -6,14 +6,48 @@ const stripe = require("stripe")(process.env.SECRET_KEY)
 const userRouter = express.Router()
 
 const storeItems = new Map([
-    ["mysQuXqVUG6yJ7qekAax", {id:"mysQuXqVUG6yJ7qekAax",name: "Basic", amount: 9.99 }],
-    [1, {id:"dL5e0aZAN2y9q0TLQq4b",name: "Standard", amount:15.49 }],
-    ["Vz0ZWl6GjahY7eQwwgY4", {name: "Premuim", amount: 20.3 }]
+    ["mysQuXqVUG6yJ7qekAax",{name: "Basic", amount: 9990 }],
+    ["dL5e0aZAN2y9q0TLQq4b",{name: "Standard", amount:1549 }],
+    ["Vz0ZWl6GjahY7eQwwgY4",{ name: "Premuim", amount: 2030 }]
 ])
 
 //Update User
-userRouter.put("/payment-checkout-session", (req, res)=>{
-
+userRouter.post("/payment-checkout-session", async(req, res)=>{
+    try {
+        const storeItem = storeItems.get(req.body.id)
+       const session =  await stripe.checkout.sessions.create({
+            payment_method_types : ['card'],
+            line_items:[{
+                price_data : {
+                        currency: 'usd',
+                        product_data:{
+                            name: storeItem.name,
+                        },
+                        unit_amount: storeItem.amount
+                },
+                quantity: 1
+            }],
+            // line_items:req.body.map(item=>{
+            //     const storeItem = storeItems.get(item.id)
+            //     return{
+            //         price_data : {
+            //             currency: 'usd',
+            //             product_data:{
+            //                 name: storeItem.name,
+            //             },
+            //             unit_amount: storeItem.amount
+            //         },
+            //             quantity: 1
+            //     }
+            // }),
+            mode: 'payment',
+            success_url:"https://httpneflixclone2://neflixclone2.com/success",
+            cancel_url:"https://neflixclone2://neflixclone2.com/failure"
+        })
+       return res.status(200).json({url:session.url})
+    } catch (error) {
+      return res.status(500).json({error: error.message})  
+    }
 })
 
 
--- index.js
+++ index.js
@@ -2,18 +2,13 @@ const express = require("express")
 const dotenv = require("dotenv")
 const helmet = require("helmet")
 const morgan = require("morgan")
-const mongoose = require("mongoose")
 const userRouter = require("./Routes/User")
 
 
 const app = express()
 dotenv.config()
 
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser:true
-},()=>{
-    console.log("Connected to Database")
-})
+
 
 app.use(express.json())
 app.use(helmet())
